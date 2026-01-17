fe3c256c297614528d52d14ee09aa1c932df7095
goft <nattasak.sudsombat@e-tech.ac.th>
2026-01-17 11:59:10 +0700

    sห

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,9 +1,22 @@
+import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
+import 'product_model.dart';
 import 'package:w3_11_nattasak/product_model.dart';
 
 class ApiService{
-  static Future <List<Product>> fetchProduct(){
+  static Future <List<Product>> fetchProduct() async{
 
-    final respo
+    final response = await http.get(Uri.parse("https://6964b225e8ce952ce1f28d47.mockapi.io/products"));
+
+    if(response.statusCode==200){
+      final List deta =  jsonDecode(response.body);
+      return deta.map((e) => Product.fromJson(e)).toList();
+
+    }else{
+      throw Exception('ไม่สำเร็จ');
+    }
   }
 }
--- lib/product_model.dart
+++ lib/product_model.dart
@@ -1,16 +1,23 @@
 class Product{
-  final String id , name, photo;
+  final String id , name, photo,description;
 
+  final double price,weight;
   Product({
     required this.id,
     required this.name,
     required this.photo,
+    required this.price,
+    required this.weight,
+    required this.description,
 });
-  factory Product.formJson(Map<String, dynamic>jsno){
+  factory Product.fromJson(Map<String, dynamic> json){
     return Product(
-        id: jsno['id'],
-        name: jsno['name'],
-        photo: jsno['photo']
+        id: json['id'],
+        name: json['name'],
+        photo: json['photo'],
+        price: (json['price'] as num).toDouble(),
+        weight: (json['weight'] as num).toDouble(),
+        description:  json['description']
     );
   }
 }
